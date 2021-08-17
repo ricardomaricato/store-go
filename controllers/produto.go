98c67c91cb0fb72899a4767d0fa8366b9166489a
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-08-17 12:48:33 -0300

    -New products page

--- controllers/produto.go
+++ controllers/produto.go
@@ -11,10 +11,10 @@ var tmpl = template.Must(template.ParseGlob("templates/*.html"))
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	todosOsProdutos := models.BuscaTodosOsProdutos()
-	template.ExecuteTemplate(w, "Index", todosOsProdutos)
+	tmpl.ExecuteTemplate(w, "Index", todosOsProdutos)
 
 }
 
 func New(w http.ResponseWriter, r *http.Request) {
-	template.ExecuteTemplate(w, "New", nil)
+	tmpl.ExecuteTemplate(w, "New", nil)
 }
