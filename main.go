999ded00a9d4c9fda7e07f65852d8a8829344e0c
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-08-16 22:42:50 -0300

    -Connecting to the database

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
+
+func conectaComBancoDeDados() *sql.DB {
+	conexao := "user=postgres dbname=root password=root host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
 
 type Produto struct {
 	Nome       string
@@ -15,6 +27,8 @@ type Produto struct {
 var tmpl = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	db := conectaComBancoDeDados()
+	defer db.Close()
 	http.HandleFunc("/", Index)
 	http.ListenAndServe(":8000", nil)
 }
