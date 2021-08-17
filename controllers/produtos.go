b308ef0bc7ff9d335b482459441803b0addfac63
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-08-17 16:29:45 -0300

    -Saving product in the bank

---
+++ controllers/produtos.go
@@ -0,0 +1,44 @@
+package controllers
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/ricardomaricato/store-go/models"
+)
+
+var tmpl = template.Must(template.ParseGlob("templates/*.html"))
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	todosOsProdutos := models.BuscaTodosOsProdutos()
+	tmpl.ExecuteTemplate(w, "Index", todosOsProdutos)
+
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertidoParaFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço:", err)
+		}
+
+		quantidadeConvertidaParaInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade:", err)
+		}
+
+		models.CriarNovoProduto(nome, descricao, precoConvertidoParaFloat, quantidadeConvertidaParaInt)
+	}
+	http.Redirect(w, r, "/", 201)
+}
--- routes/routes.go
+++ routes/routes.go
@@ -6,9 +6,8 @@ import (
 	"github.com/ricardomaricato/store-go/controllers"
 )
 
-//comentário
-
 func CarregaRotas() {
 	http.HandleFunc("/", controllers.Index)
 	http.HandleFunc("/new", controllers.New)
+	http.HandleFunc("/insert", controllers.Insert)
 }
